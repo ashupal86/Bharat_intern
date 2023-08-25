ed122013d2f0ff52dc9e70384b17d26413a13d67
Ashu <palbro86@gmail.com>
2023-08-25 23:00:17 +0530

    textview is adding button text by java function

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,17 +3,37 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
+public TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn=findViewById(R.id.equal);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
+
+    public void tap(View view){
+        MaterialButton btn=(MaterialButton) view;
+        TextView tv=findViewById(R.id.textView);
+
+        tv.setText(tv.getText()+btn.getText().toString());
+    }
+
+
+
 }
